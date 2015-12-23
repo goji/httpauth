723e9a2b49655f1d067eb9e197b874df3bca973f
Cameron Moore <moorereason@gmail.com>
2015-12-23 16:34:07 -0600

    Add custom authentication function feature
    This commit adds a new AuthFunc field to the AuthOptions struct.  The User and
    Password fields are left in-place for backwards compatibility.  This construct
    shouldn't break any user code unless they are using unlabeled struct literals,
    in which case it should break on principle since you shouldn't do that on
    structs outside of your control.

--- basic_auth_test.go
+++ basic_auth_test.go
@@ -6,6 +6,58 @@ import (
 	"testing"
 )
 
+func TestBasicAuthAuthenticateWithFunc(t *testing.T) {
+	requiredUser := "jqpublic"
+	requiredPass := "secret.sauce"
+
+	// Dumb test function
+	fn := func(u, p string) bool {
+		return u == requiredUser && p == requiredPass
+	}
+
+	// Provide a minimal test implementation.
+	authOpts := AuthOptions{
+		Realm:    "Restricted",
+		AuthFunc: fn,
+	}
+
+	b := &basicAuth{opts: authOpts}
+
+	r := &http.Request{Method: "GET"}
+
+	if b.authenticate(nil) {
+		t.Fatal("Should not succeed when http.Request is nil")
+	}
+
+	// Provide auth data, but no Authorization header
+	if b.authenticate(r) != false {
+		t.Fatal("No Authorization header supplied.")
+	}
+
+	// Initialise the map for HTTP headers
+	r.Header = http.Header(make(map[string][]string))
+
+	// Set a malformed/bad header
+	r.Header.Set("Authorization", "    Basic")
+	if b.authenticate(r) != false {
+		t.Fatal("Malformed Authorization header supplied.")
+	}
+
+	// Test correct credentials
+	auth := base64.StdEncoding.EncodeToString([]byte("jqpublic:secret.sauce"))
+	r.Header.Set("Authorization", "Basic "+auth)
+	if b.authenticate(r) != true {
+		t.Fatal("Failed on correct credentials")
+	}
+
+	// Test incorrect credentials
+	auth = base64.StdEncoding.EncodeToString([]byte("jqpublic:hackydoo"))
+	r.Header.Set("Authorization", "Basic "+auth)
+	if b.authenticate(r) == true {
+		t.Fatal("Success when expecting failure")
+	}
+}
+
 func TestBasicAuthAuthenticate(t *testing.T) {
 	// Provide a minimal test implementation.
 	authOpts := AuthOptions{
@@ -18,8 +70,7 @@ func TestBasicAuthAuthenticate(t *testing.T) {
 		opts: authOpts,
 	}
 
-	r := &http.Request{}
-	r.Method = "GET"
+	r := &http.Request{Method: "GET"}
 
 	// Provide auth data, but no Authorization header
 	if b.authenticate(r) != false {
@@ -43,10 +94,23 @@ func TestBasicAuthAuthenticate(t *testing.T) {
 	}
 }
 
-func TestBasicAuthAutenticateWithouUserAndPass(t *testing.T) {
+func TestBasicAuthAuthenticateWithoutUserAndPass(t *testing.T) {
 	b := basicAuth{opts: AuthOptions{}}
 
-	if b.authenticate(nil) {
-		t.Fatal("Should not authenticate if user or pass are not set on opts")
+	r := &http.Request{Method: "GET"}
+
+	// Provide auth data, but no Authorization header
+	if b.authenticate(r) != false {
+		t.Fatal("No Authorization header supplied.")
+	}
+
+	// Initialise the map for HTTP headers
+	r.Header = http.Header(make(map[string][]string))
+
+	// Test correct credentials
+	auth := base64.StdEncoding.EncodeToString([]byte(b.opts.User + ":" + b.opts.Password))
+	r.Header.Set("Authorization", "Basic "+auth)
+	if b.authenticate(r) != false {
+		t.Fatal("Success when expecting failure")
 	}
 }
